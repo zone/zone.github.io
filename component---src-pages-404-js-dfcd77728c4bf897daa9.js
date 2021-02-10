6c8f5d29d4991615aaa505a2b8fa1f1523e01df0
Stuart Burrows <sburrows@zonedigital.com>
2021-02-10 17:01:24 +0000

    Updates

--- component---src-pages-404-js-dfcd77728c4bf897daa9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(t,n,e){"use strict";e.r(n),e.d(n,"default",(function(){return s}));e("q1tI");var o=e("KE4F"),u=e("7oih"),i=e("qKvR");function s(){return Object(i.b)(u.a,{title:"404 - Not found",description:"Sorry this page cannot be found."},Object(i.b)(o.a,null,Object(i.b)("h1",null,"NOT FOUND"),Object(i.b)("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-dfcd77728c4bf897daa9.js.map
